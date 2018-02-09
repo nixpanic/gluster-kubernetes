5485b9f3bf7dc05b48b9d5187c6ee785cca6bf24
Niels de Vos <ndevos@redhat.com>
2018-02-09 18:46:23 +0100

    [WIP/WORKS] single-node: Vagrant environment running Kubespray and gk-deploy
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

--- kubespray/setup-kubespray.sh
+++ kubespray/setup-kubespray.sh
@@ -1,27 +1,44 @@
 #!/bin/sh
+#
+# Test environment for a single-node Heketi deployment.
+#
+# This script basically does the following:
+# - use kubespray to install a single node kubernetes cluster
+# - git clone the gk-deploy repository and run it
+#
+# Author: Niels de Vos <ndevos@redhat.com>
+#
 
-yum -y install git ansible python-netaddr
-
-hostnamectl set-hostname node1
-swapoff -a
-sed -i '/swap/d' /etc/fstab
-
-mkdir $HOME/bin || true
-[ -x $HOME/bin/kubectl ] || curl -Lo $HOME/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x $HOME/bin/kubectl
-
-git clone https://github.com/kubernetes-incubator/kubespray
-pushd kubespray
+# needs mount.glusterfs on the container host, in a recent version
+yum -y install centos-release-gluster
+yum -y install git ansible python-netaddr glusterfs-fuse
 
 # kubespray needs python-jinja2 >= 2.9, not available as RPM :-(
 yum -y install python-setuptools
 easy_install pip
 pip install 'jinja2 >= 2.9'
 
+# the default hostname from the kubespray repository is "node1"
+hostnamectl set-hostname node1
+
+# kubelet does not like swap
+swapoff -a
+sed -i '/swap/d' /etc/fstab
+
+# the kubectl from CentOS is not recent enough
+mkdir $HOME/bin || true
+[ -x $HOME/bin/kubectl ] || curl -Lo $HOME/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x $HOME/bin/kubectl
+
+# go run Kubespray
+git clone https://github.com/kubernetes-incubator/kubespray
+pushd kubespray
+
 # eventhough swap is disabled, the assertion on it fails, set ignore_assert_errors=True
 ansible-playbook -i inventory/local/hosts.ini -e ignore_assert_errors=True cluster.yml
 
 popd
 
+# go and run gk-deploy
 git clone https://github.com/gluster/gluster-kubernetes
 pushd gluster-kubernetes/deploy
 
